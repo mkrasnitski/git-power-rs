736b44340c249c0064133676f4a8ad7a711f9dbc
Michael Krasnitski <michael.krasnitski@gmail.com>
2021-07-06 18:53:57 -0400

    replace libgit's sha1 impl with a faster one

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,92 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use git2::{ObjectType, Oid, Repository, ResetType};
+use sha1::{Digest, Sha1};
+use std::fmt;
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 use std::sync::{mpsc, Arc};
+
+const NONCE_LENGTH: usize = 32;
+
+#[derive(Clone)]
+struct CommitBuffer {
+    buf: String,
+    header_len: usize,
+    nonce_start: usize,
+    nonce_end: usize,
+}
+
+impl CommitBuffer {
+    fn new(buf: &[u8]) -> Result<Self> {
+        let mut buf = String::from_utf8(buf.iter().cloned().collect())?;
+        let start = match buf.find("-----BEGIN PGP SIGNATURE-----") {
+            Some(pgp_idx) => match buf[pgp_idx..].find("Nonce") {
+                Some(idx) => pgp_idx + idx + 7,
+                None => {
+                    let pgp_header_end = pgp_idx
+                        + buf[pgp_idx..]
+                            .find("\n ")
+                            .ok_or("Malformed PGP header")
+                            .map_err(Error::msg)?;
+                    buf.insert_str(pgp_header_end, "\nNonce: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                    pgp_header_end + 8
+                }
+            },
+            None => {
+                let header_end = buf
+                    .find("\n\n")
+                    .ok_or("Malformed commit")
+                    .map_err(Error::msg)?;
+                match buf.find("nonce") {
+                    Some(idx) => idx + 6,
+                    None => {
+                        buf.insert_str(header_end, "\nnonce AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                        header_end + 7
+                    }
+                }
+            }
+        };
+        let line_end = start
+            + buf[start..]
+                .find("\n")
+                .ok_or("Malformed Nonce")
+                .map_err(Error::msg)?;
+        if line_end - start != NONCE_LENGTH {
+            buf.replace_range(start..line_end, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+        let commit_header = format!("commit {}\0", buf.len());
+        buf.insert_str(0, &commit_header);
+        let header_len = commit_header.len();
+        Ok(Self {
+            buf,
+            header_len,
+            nonce_start: start + header_len,
+            nonce_end: start + NONCE_LENGTH + header_len,
+        })
+    }
+
+    fn write_nonce(&mut self, val: u128) {
+        // Only covers 128/160 bits of entropy
+        // possible chars: ABCDEFGHIJKLMNOP
+        let mut nonce_bytes = [b'A'; NONCE_LENGTH];
+        for i in 0..NONCE_LENGTH {
+            let idx = (val >> (4 * i)) & 0xF;
+            nonce_bytes[31 - i] = b'A' + idx as u8;
+        }
+        // SAFETY: The nonce is always valid ASCII
+        unsafe { self.buf[self.nonce_start..self.nonce_end].as_bytes_mut() }
+            .copy_from_slice(&nonce_bytes);
+    }
+
+    fn bytes(&self) -> &[u8] {
+        self.buf[self.header_len..].as_bytes()
+    }
+}
+
+impl fmt::Display for CommitBuffer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", &self.buf[self.header_len..])
+    }
+}
 
 fn num_leading_zero_bits(oid: &Oid) -> u32 {
     let mut zeros = 0;
@@ -14,56 +99,7 @@ fn num_leading_zero_bits(oid: &Oid) -> u32 {
     zeros
 }
 
-fn nonce_bytes(nonce: u128) -> [u8; 32] {
-    let mut nonce_bytes = [b'A'; 32];
-    // only covers 128/160 bits of entropy
-    // possible chars: ABCDEFGHIJKLMNOP
-    for i in 0..32 {
-        let idx = (nonce >> (4 * i)) & 0xF;
-        nonce_bytes[31 - i] = b'A' + idx as u8;
-    }
-    nonce_bytes
-}
-
-fn write_nonce(buf: &mut String, start: usize, val: u128, first: &mut bool) {
-    let nonce_bytes = nonce_bytes(val);
-    let end = if *first {
-        *first = false;
-        start + buf[start..].find("\n").unwrap()
-    } else {
-        start + 32
-    };
-    if end - start != 32 {
-        let nonce_string: String = nonce_bytes.iter().map(|c| *c as char).collect();
-        buf.replace_range(start..end, &nonce_string);
-    } else {
-        // SAFETY: The nonce is always valid UTF-8
-        unsafe { buf[start..end].as_bytes_mut() }.copy_from_slice(&nonce_bytes);
-    }
-}
-
-fn try_commit(mut buf: String, target_zeros: u32) -> Result<(String, Oid)> {
-    let start = match buf.find("-----BEGIN PGP SIGNATURE-----") {
-        Some(pgp_idx) => match buf[pgp_idx..].find("Nonce") {
-            Some(idx) => pgp_idx + idx + 7,
-            None => {
-                let pgp_header_end = pgp_idx + buf[pgp_idx..].find("\n ").unwrap();
-                buf.insert_str(pgp_header_end, "\nNonce: AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-                pgp_header_end + 8
-            }
-        },
-        None => {
-            let header_end = buf.find("\n\n").unwrap();
-            match buf.find("nonce") {
-                Some(idx) => idx + 6,
-                None => {
-                    buf.insert_str(header_end, "\nnonce AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-                    header_end + 7
-                }
-            }
-        }
-    };
-
+fn try_commit(commit: CommitBuffer, target_zeros: u32) -> Result<(CommitBuffer, Oid)> {
     let (tx, rx) = mpsc::channel();
     let stop = Arc::new(AtomicBool::new(false));
     let num_hashes = Arc::new(AtomicU64::new(0));
@@ -74,28 +110,31 @@ fn try_commit(mut buf: String, target_zeros: u32) -> Result<(String, Oid)> {
         let tx = tx.clone();
         let stop = Arc::clone(&stop);
         let num_hashes = Arc::clone(&num_hashes);
-        let mut buf = buf.clone();
-        std::thread::spawn(move || {
-            let mut first = true;
+        let mut commit = commit.clone();
+        std::thread::spawn(move || -> Result<()> {
+            let mut hasher = Sha1::new();
             for nonce in i * chunk_size..(i + 1) * chunk_size {
                 if stop.load(Ordering::Relaxed) {
                     break;
                 }
                 num_hashes.fetch_add(1, Ordering::Relaxed);
-                write_nonce(&mut buf, start, nonce, &mut first);
-                let hash = Oid::hash_object(ObjectType::Commit, buf.as_bytes()).unwrap();
+                commit.write_nonce(nonce);
+                hasher.update(&commit.buf);
+                let hash = Oid::from_bytes(hasher.finalize_reset().as_slice())?;
                 let zeros = num_leading_zero_bits(&hash);
                 if zeros >= target_zeros {
-                    tx.send(buf).unwrap();
+                    println!("{}", nonce);
+                    tx.send(commit)?;
                     break;
                 }
             }
+            Ok(())
         });
     }
     let buf = rx.recv()?;
     stop.store(true, Ordering::Relaxed);
 
-    let hash = Oid::hash_object(ObjectType::Commit, buf.as_bytes())?;
+    let hash = Oid::hash_object(ObjectType::Commit, buf.bytes())?;
     println!(
         "{} {}",
         num_hashes.load(Ordering::Relaxed),
@@ -106,15 +145,18 @@ fn try_commit(mut buf: String, target_zeros: u32) -> Result<(String, Oid)> {
 }
 
 fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        return Err(Error::msg(format!("Usage: {} [num-zeros]", args[0])));
+    }
     let repo = Repository::open(std::env::current_dir()?)?;
     let head_commit_hash = repo.head()?.peel_to_commit()?.id();
     let odb = repo.odb()?;
-    let commit_data = odb.read(head_commit_hash)?.data().iter().cloned().collect();
-    let buf = String::from_utf8(commit_data)?;
+    let buf = CommitBuffer::new(odb.read(head_commit_hash)?.data())?;
 
-    let target_zeros = 28;
-    let (buf, hash) = try_commit(buf.clone(), target_zeros)?;
-    odb.write(ObjectType::Commit, buf.as_bytes())?;
+    let target_zeros = args[1].parse()?;
+    let (buf, hash) = try_commit(buf, target_zeros)?;
+    odb.write(ObjectType::Commit, buf.bytes())?;
     repo.reset(&repo.find_object(hash, None)?, ResetType::Soft, None)?;
     println!("found {}", hash);
     Ok(())
