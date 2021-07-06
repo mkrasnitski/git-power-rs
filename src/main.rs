3f9090dcee5882bb4492478490b4ffab9bb609ea
Michael Krasnitski <michael.krasnitski@gmail.com>
2021-07-06 04:14:18 -0400

    use rayon for multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use anyhow::Result;
-use git2::{Commit, ObjectType, Oid, Repository, ResetType};
+use anyhow::{Error, Result};
+use git2::{ObjectType, Oid, Repository, ResetType};
+use rayon::prelude::*;
 
 fn num_leading_zero_bits(oid: &Oid) -> u32 {
     let mut zeros = 0;
@@ -23,15 +24,12 @@ fn nonce_string(nonce: u128) -> String {
     nonce_string
 }
 
-fn try_commit(repo: &Repository, c: &Commit, target_zeros: u32) -> Result<Oid> {
-    if num_leading_zero_bits(&c.id()) >= target_zeros {
-        return Ok(c.id());
-    }
+fn write_nonce(buf: &mut String, start: usize, val: u128) {
+    let end = start + buf[start..].find("\n").unwrap();
+    buf.replace_range(start..end, &nonce_string(val));
+}
 
-    let odb = repo.odb()?;
-    let mut buf = String::from_utf8(odb.read(c.id())?.data().iter().copied().collect())?;
-
-    let mut nonce = 0;
+fn try_commit(mut buf: String, target_zeros: u32) -> Result<(String, Oid)> {
     let nonce_start = match buf.find("-----BEGIN PGP SIGNATURE-----") {
         Some(pgp_idx) => match buf.find("Nonce") {
             Some(idx) => idx + 7,
@@ -51,28 +49,39 @@ fn try_commit(repo: &Repository, c: &Commit, target_zeros: u32) -> Result<Oid> {
         },
     };
 
-    loop {
-        let nonce_end = nonce_start + buf[nonce_start..].find("\n").unwrap();
-        buf.replace_range(nonce_start..nonce_end, &nonce_string(nonce));
-        let hash = Oid::hash_object(ObjectType::Commit, buf.as_bytes())?;
-        let zeros = num_leading_zero_bits(&hash);
-        if zeros >= target_zeros {
-            println!("{} {}", nonce, zeros);
-            println!("{}", buf);
-            odb.write(ObjectType::Commit, &buf.as_bytes())?;
-            let object = repo.find_object(hash, None)?;
-            repo.reset(&object, ResetType::Soft, None)?;
-            return Ok(hash);
-        }
+    let good_nonce = (0..u128::MAX)
+        .into_par_iter()
+        .find_any(|&nonce| {
+            let mut buf = buf.clone();
+            write_nonce(&mut buf, nonce_start, nonce);
+            let hash = Oid::hash_object(ObjectType::Commit, buf.as_bytes()).unwrap();
+            let zeros = num_leading_zero_bits(&hash);
+            zeros >= target_zeros
+        })
+        .ok_or("Hash not found")
+        .map_err(Error::msg)?;
 
-        nonce += 1;
-    }
+    write_nonce(&mut buf, nonce_start, good_nonce);
+    let hash = Oid::hash_object(ObjectType::Commit, buf.as_bytes())?;
+    println!("{} {}", good_nonce, num_leading_zero_bits(&hash));
+    println!("{}", buf);
+    Ok((buf, hash))
 }
 
 fn main() -> Result<()> {
     let repo = Repository::open(std::env::current_dir()?)?;
-    let head_commit = repo.head()?.peel_to_commit()?;
-    let hash = try_commit(&repo, &head_commit, 24)?;
-    println!("found {}", hash);
+    let head_commit_hash = repo.head()?.peel_to_commit()?.id();
+    let odb = repo.odb()?;
+    let commit_data = odb.read(head_commit_hash)?.data().iter().cloned().collect();
+    let buf = String::from_utf8(commit_data)?;
+
+    let target_zeros = 32;
+    if num_leading_zero_bits(&head_commit_hash) < target_zeros {
+        let (buf, hash) = try_commit(buf.clone(), target_zeros)?;
+        odb.write(ObjectType::Commit, buf.as_bytes())?;
+        let object = repo.find_object(hash, None)?;
+        repo.reset(&object, ResetType::Soft, None)?;
+        println!("found {}", hash);
+    }
     Ok(())
 }
