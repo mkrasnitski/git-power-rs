251bb4ca6e66b03b3b41e0912cf6e96f8ea00ed3
Michael Krasnitski <michael.krasnitski@gmail.com>
2021-07-05 20:15:13 -0400

    performance improvements
    Take commit_create_buffer out of the hotpath, and only create the buffer
    once. Then we do string operations on it throught the CommitBufferStr
    wrapper type.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::{Error, Result};
-use git2::{Commit, ObjectType, Oid, Repository, ResetType, Signature, Time};
+use git2::{Buf, Commit, ObjectType, Oid, Repository, ResetType, Time};
+use std::io::Write;
+use std::ops::Deref;
 
 const TZ_OFFSETS: [i32; 38] = [
     -720, -660, -600, -570, -540, -480, -420, -360, -300, -240, -210, -180, -120, -60, 0, 60, 120,
@@ -19,6 +21,72 @@ impl<'a> ValidUTF8<&'a str> for Option<&'a str> {
     }
 }
 
+fn timestamp_string(time: i64, offset: i32) -> String {
+    let t = Time::new(time, offset);
+    let offset = t.offset_minutes().abs();
+    format!(
+        "{} {}{:02}{:02}",
+        t.seconds(),
+        t.sign(),
+        offset / 60,
+        offset % 60
+    )
+}
+
+struct CommitBufferStr<'repo> {
+    repo: &'repo Repository,
+    buf: String,
+}
+
+impl<'repo> CommitBufferStr<'repo> {
+    fn new(repo: &'repo Repository, b: Buf) -> Result<Self> {
+        Ok(Self {
+            repo,
+            buf: b.as_str().valid_utf8()?.to_owned(),
+        })
+    }
+
+    fn author_timestamp(&mut self) -> &mut [u8] {
+        let start = self.buf.find('>').unwrap() + 2;
+        let end = start + self.buf[start..].find('\n').unwrap();
+        // SAFETY: The timestamp will always be valid UTF-8
+        unsafe { self.buf[start..end].as_bytes_mut() }
+    }
+
+    fn set_author_timestamp(&mut self, time: i64, offset: i32) {
+        self.author_timestamp()
+            .copy_from_slice(timestamp_string(time, offset).as_bytes());
+    }
+
+    fn committer_timestamp(&mut self) -> &mut [u8] {
+        let start = self.buf.find('>').unwrap() + 2;
+        let start = start + self.buf[start..].find('>').unwrap() + 2;
+        let end = start + self.buf[start..].find('\n').unwrap();
+        // SAFETY: The timestamp will always be valid UTF-8
+        unsafe { self.buf[start..end].as_bytes_mut() }
+    }
+
+    fn set_committer_timestamp(&mut self, time: i64, offset: i32) {
+        self.committer_timestamp()
+            .copy_from_slice(timestamp_string(time, offset).as_bytes());
+    }
+
+    fn write_to_odb(&self) -> Result<()> {
+        let odb = self.repo.odb()?;
+        let mut writer = odb.writer(self.buf.len(), ObjectType::Commit)?;
+        writer.write(&self.buf.as_bytes())?;
+        writer.finalize()?;
+        Ok(())
+    }
+}
+
+impl Deref for CommitBufferStr<'_> {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.buf
+    }
+}
+
 fn num_leading_zero_bits(oid: &Oid) -> u32 {
     let mut zeros = 0;
     for &byte in oid.as_bytes() {
@@ -33,48 +101,36 @@ fn num_leading_zero_bits(oid: &Oid) -> u32 {
 fn try_commit(repo: &Repository, c: &Commit, target_zeros: u32) -> Result<Oid> {
     let author = c.author();
     let committer = c.committer();
-    let message = c.message().valid_utf8()?;
-    let tree = c.tree()?;
-    let parents = c.parents().collect::<Vec<Commit>>();
+    let mut buf = CommitBufferStr::new(
+        &repo,
+        repo.commit_create_buffer(
+            &author,
+            &committer,
+            &c.message().valid_utf8()?,
+            &c.tree()?,
+            c.parents()
+                .collect::<Vec<Commit>>()
+                .iter()
+                .collect::<Vec<&Commit>>()
+                .as_slice(),
+        )?,
+    )?;
 
     let mut author_offset_idx = 0;
     let mut committer_offset_idx = 0;
-    let author_timestamp = author.when().seconds();
-    let mut committer_timestamp = committer.when().seconds();
-    println!("initial timestamp: {}", author_timestamp);
+    let author_ts = author.when().seconds();
+    let mut committer_ts = author_ts;
 
     let mut num_hashes = 0;
     loop {
-        let author_sig = Signature::new(
-            author.name().valid_utf8()?,
-            author.email().valid_utf8()?,
-            &Time::new(author_timestamp, TZ_OFFSETS[author_offset_idx]),
-        )?;
-        let committer_sig = Signature::new(
-            committer.name().valid_utf8()?,
-            committer.email().valid_utf8()?,
-            &Time::new(committer_timestamp, TZ_OFFSETS[committer_offset_idx]),
-        )?;
-        let buf = repo.commit_create_buffer(
-            &author_sig,
-            &committer_sig,
-            &message,
-            &tree,
-            parents.iter().collect::<Vec<&Commit>>().as_slice(),
-        )?;
-        let hash = Oid::hash_object(ObjectType::Commit, &buf)?;
+        buf.set_author_timestamp(author_ts, TZ_OFFSETS[author_offset_idx]);
+        buf.set_committer_timestamp(committer_ts, TZ_OFFSETS[committer_offset_idx]);
+        let hash = Oid::hash_object(ObjectType::Commit, buf.as_bytes())?;
         let zeros = num_leading_zero_bits(&hash);
         if zeros >= target_zeros {
             println!("{} {}", num_hashes, zeros);
-            println!("{}", buf.as_str().valid_utf8()?);
-            repo.commit(
-                None,
-                &author_sig,
-                &committer_sig,
-                &message,
-                &tree,
-                parents.iter().collect::<Vec<&Commit>>().as_slice(),
-            )?;
+            println!("{}", &*buf);
+            buf.write_to_odb()?;
             let object = repo.find_object(hash, None)?;
             repo.reset(&object, ResetType::Soft, None)?;
             return Ok(hash);
@@ -88,7 +144,7 @@ fn try_commit(repo: &Repository, c: &Commit, target_zeros: u32) -> Result<Oid> {
         }
         if committer_offset_idx == TZ_OFFSETS.len() {
             committer_offset_idx = 0;
-            committer_timestamp += 1;
+            committer_ts += 1;
         }
     }
 }
@@ -96,7 +152,7 @@ fn try_commit(repo: &Repository, c: &Commit, target_zeros: u32) -> Result<Oid> {
 fn main() -> Result<()> {
     let repo = Repository::open(std::env::current_dir()?)?;
     let head_commit = repo.head()?.peel_to_commit()?;
-    let hash = try_commit(&repo, &head_commit, 16)?;
+    let hash = try_commit(&repo, &head_commit, 24)?;
     println!("found {}", hash);
     Ok(())
 }
